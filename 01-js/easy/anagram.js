652ed0e01913a8673a12acd720cc7e076f19d88e
Vaibhav Matere <vpm00718897@gmail.com>
2023-12-12 12:35:58 +0530

    anagram solution added

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,14 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if (str1.sort() == str2.sort()) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+var ans = isAnagram("rasp", "pasr");
+console.log(ans);
 
 module.exports = isAnagram;
